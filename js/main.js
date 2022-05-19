d476a02ac5a809e7ea138575afd155d8cd329291
cookie.729 <cookie.729@icloud.com>
2022-05-19 21:46:47 +0900

    photographer-fumika

--- js/main.js
+++ js/main.js
@@ -7,9 +7,3 @@ const nav = document.querySelector('.category-nav');
 category.addEventListener('click', () => {
   nav.classList.toggle('open-menu');
 });
-
-// const btn = document.querySelector('.btn-menu');
-// const nav = document.querySelector('.main-nav');
-// btn.addEventListener('click', () => {
-//   nav.classList.toggle('open-menu');
-// });
