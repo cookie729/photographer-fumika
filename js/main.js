da3c7c699fbce2b6ebe5f3b4a15f591de35bf44e
cookie.729 <cookie.729@icloud.com>
2022-05-27 20:55:17 +0900

    photographer-fumika

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
+
 'use strict'
+
+
+new LuminousGallery(document.querySelectorAll(".gallery"));
+
+const category = document.querySelector('.category-menu');
+const nav = document.querySelector('.category-nav');
+category.addEventListener('click', () => {
+  nav.classList.toggle('open-menu');
+});
 
 const btn = document.querySelector('.btn-menu');
 const mobile_nav = document.querySelector('.mobile-main-nav');
 btn.addEventListener('click', () => {
   mobile_nav.classList.toggle('open-menu');
 });
-new LuminousGallery(document.querySelectorAll(".gallery"));
-
 
 const mobile_category = document.querySelector('.mobile-category-menu');
 const mobile_category_nav = document.querySelector('.mobile-category-nav');
 mobile_category.addEventListener('click', () => {
   mobile_category_nav.classList.toggle('open-menu');
-});
-
-const category = document.querySelector('.category-menu');
-const nav = document.querySelector('.category-nav');
-category.addEventListener('click', () => {
-  nav.classList.toggle('open-menu');
 });
